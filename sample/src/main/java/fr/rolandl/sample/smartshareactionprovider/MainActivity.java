73504a489d3b6198ca8f4d0c7ed0d48886039049
LudovicSmartnSoft <ludovic@smartnsoft.com>
2015-01-02 23:21:32 +0100

    Initial commit. Added the project and modules structure.

---
+++ sample/src/main/java/fr/rolandl/sample/smartshareactionprovider/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.rolandl.sample.smartshareactionprovider;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * @author Ludovic Roland
+ * @since 2015.01.02
+ */
+public class MainActivity
+    extends ActionBarActivity
+{
+
+}
